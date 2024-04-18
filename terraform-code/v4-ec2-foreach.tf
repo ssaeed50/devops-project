72e190463720ec60d2564fc564dff672720e4d8f
ssaeed <shehab.saeed50@gmail.com>
2024-04-18 03:44:10 +0200

    added jenkins port to new security group

--- terraform-code/v4-ec2-foreach.tf
+++ terraform-code/v4-ec2-foreach.tf
@@ -34,8 +34,16 @@ resource "aws_security_group" "allow_SSH" {
   }
 
   ingress {
+    description = "SSH port"
     from_port        = 22
     to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "Jenkins port"
+    from_port        = 8080
+    to_port          = 8080
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
   }
